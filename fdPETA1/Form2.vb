6d48ec6f17e12eeebb69ad1613a3aa709a9d4e27
lain <corellon.larethian25@gmail.com>
2024-09-11 22:06:39 +0800

    Add files via upload

---
+++ fdPETA1/Form2.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Hide()
+        Form1.Show()
+    End Sub
+End Class
---
+++ fdPETA1/Form3.Designer.vb
@@ -0,0 +1,39 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' Form3
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources._43f57efb46d87b9c6f654968e7c0fce5
+        BackgroundImageLayout = ImageLayout.Zoom
+        ClientSize = New Size(400, 387)
+        Cursor = Cursors.Default
+        Name = "Form3"
+        Text = "awooga"
+        ResumeLayout(False)
+    End Sub
+End Class
